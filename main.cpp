bac8897f075c9122e20eb3418343091b2c909cce
arima-naoto <k023g1010@g.neec.ac.jp>
2025-02-26 01:13:46 +0900

    DirectX12の基盤を作成した

--- main.cpp
+++ main.cpp
@@ -1,8 +1,30 @@
-#include <Windows.h>
+﻿#include "WinApp.h"
+#include "DirectXCommon.h"
 
+// Windowアプリのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
+	WinApp* win = nullptr;
+	DirectXCommon* dxCommon = nullptr;
 
+	win = WinApp::GetInstance();
+	win->CreateGameWindow();
 
+	dxCommon = DirectXCommon::GetInstance();
+	dxCommon->Initialize(win);
+
+	while (true) {
+		if (win->ProcessMessage()) {
+			break;
+		}
+
+		dxCommon->BeginDraw();
+
+		dxCommon->EndDraw();
+
+	}
+
+	
+	win->TerminateGameWindow();
 	return 0;
 }
